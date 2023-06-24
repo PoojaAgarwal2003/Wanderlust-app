fad5ee1752140bf06ba6777508774dbd47dbb635
PoojaAgarwal2003 <poojasps2003@gmail.com>
2023-06-24 09:54:32 +0530

    backend complete

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports={
+    MONGOURI:"mongodb+srv://PoojaAgarwal:2DcVOCzYNCygtyTa@cluster0.nxhfhtj.mongodb.net/?retryWrites=true&w=majority",
+    // 2DcVOCzYNCygtyTa
+    JWT_SECRET: "poojaagarwal"
+}
